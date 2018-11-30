0c15dc7f489879dba3d865d265bcd640be711a26
83chameleon <1187307315@qq.com>
2018-11-30 22:42:57 +0800

    danmaku

---
+++ src/components/Course/lessonPlayer/DPlayer.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <div id="dplayer"></div>
+    </div>
+</template>
+
+<script>
+    import 'dplayer/dist/DPlayer.min.css';
+    import DPlayer from 'dplayer';
+
+    export default {
+        components:{
+            'd-player': DPlayer,
+        },
+        name: "dplayer",
+        data(){
+            return{
+                options: Object,
+            }
+        },
+        mounted(){
+            this.initPlayer();
+        },
+        methods:{
+            initPlayer(){
+                this.options = new DPlayer({
+                    container: document.getElementById('dplayer'),
+                    video:{
+                        url: 'http://172.17.21.94:8080/media/test.44badb44.mp4',
+                    },
+                    screenshot: false,
+                    autoplay: false,
+                    // contextmenu: [
+                    //
+                    // ],
+                    danmaku: {
+                        id: this.$route.params.lessonId,
+                        api: 'http://39.106.156.178:1207/',
+                        maximum: 1000,
+                        user: this.$store.getters.getStorge.user.id,
+                        bottom: '15%',
+                        unlimited: true
+                    },
+                });
+            },
+            play(){
+                console.log('play callback')
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
